12950a0249d0ef19d56601c48a5db1a570a0fcaf
Danilomr305 <danilomr305@gmail.com>
2025-06-29 21:23:07 -0300

    Repository Task Criado

---
+++ src/main/java/com/danilomr/todosimples/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.danilomr.todosimples.repository;
+
+import com.danilomr.todosimples.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_id(Long id);
+}
