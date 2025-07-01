b18a12c2226b72ad201b9967e69c8496e8720a82
Danilomr305 <danilomr305@gmail.com>
2025-07-01 19:37:48 -0300

    WebConfing Ja cria e configurada para ta aceitando a conecxão do back e front

--- src/main/java/com/danilomr/todosimples/config/WebConfing.java
+++ src/main/java/com/danilomr/todosimples/config/WebConfing.java
@@ -1,4 +1,15 @@
 package com.danilomr.todosimples.config;
 
-public class WebConfing {
-}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfing implements WebMvcConfigurer {
+
+    public  void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/**"); //esse codigo serve para libera qualquer mepiamento, get,pos, put e del
+    }                                         //mas se eu quiser codificar qualquer segurança é so coloca um "."
+}                                             //depos do Ex: {("/**"). }
